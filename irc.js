1cbbdc94c22bd3d24ef33f62986d9ff46f4d1fab
Mark Randles <randlem@gmail.com>
2013-07-06 23:45:01 -0500

    Responding to pings

--- irc.js
+++ irc.js
@@ -1,41 +1,70 @@
 var _ = require('underscore');
 var util = require('util');
 
-function cmd(c, opts) {
-	if (opts) {
-		var o = _.reduce(opts, function(m, i) {
-			console.log(m, i);
-			return m.concat(' ', i);
-		}, '').trim();
-	}
-	fmt = util.format('%s %s\r\n', c, o);
-	console.log(fmt);
-	return fmt;
-}
-
 exports.irc = Irc;
 function Irc(options) {
 	var self = {
-		user: '',
-		pass: ''
+		user: ''
 	};
 
 	_.extend(self, options)
+
+	function collapse(ary) {
+		return _.reduce(ary, function(m, i) {
+			return m.concat(' ', i);
+		}, '').trim();
+	}
+
+	function cmd(c, opts) {
+		if (opts) {
+			o = collapse(opts);
+		}
+		fmt = util.format('%s %s\r\n', c, o);
+		console.log(fmt.trim());
+		return fmt;
+	}
+
+	function handleMessage(str) {
+		var payload = str.split(' ');
+		var source  = '';
+		if (payload[0].substr(0,1) == ':') {
+			source = payload.shift();
+		}
+		var msg = payload.shift();
+		switch (msg) {
+			case 'PING':
+				self.socket.write(cmd('PONG', payload))
+				break;
+			case 'PRIVMSG':
+				var to = payload.shift();
+				var msg = collapse(payload);
+				console.log('Private message to %s =  %s', to, msg);
+				break;
+		}
+	}
 
 	self.start = function(socket) {
 		self.socket = socket;
 		socket.write(cmd('NICK', [self.user]));
 		socket.write(cmd('USER', [self.user, 0, '*', ':Sisbot']));
+		if (self.channels.lenght > 0) {
+			_.each(self.channels, function(channel) {
+				socket.write('JOIN', [channel]);
+			});
+		}
 	};
 
 	self.end = function() {
 		console.log('on disconnect');
 	};
 
-	var i = 0;
 	self.recv = function(data) {
-		console.log('%s: data', i, data.toString());
-		i++;
+		_.each(data.toString().split('\n'), function(line) {
+			if (line.trim().length > 0) {
+				console.log(line);
+				handleMessage(line);
+			}
+		});
 	};
 
 	return self;
